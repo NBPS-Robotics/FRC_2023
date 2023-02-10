fc80d1c0fdb699f80b34423e82d34dbf1a15a4fb
Kelin-Levine <kelin_levine@mynbps.org>
2023-02-10 12:17:07 -0500

    joystick control to srx encoders 6-9

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
@@ -31,22 +35,42 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   
-  public final static CANSparkMax m_frontLeft = new CANSparkMax(1, MotorType.kBrushless);
-  public final static CANSparkMax m_frontRight = new CANSparkMax(2, MotorType.kBrushless);
-  public final static CANSparkMax m_backLeft = new CANSparkMax(3, MotorType.kBrushless);
-  public final static CANSparkMax m_backRight = new CANSparkMax(4, MotorType.kBrushless);
+  //public final static CANSparkMax m_frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+  //public final static CANSparkMax m_frontRight = new CANSparkMax(2, MotorType.kBrushless);
+  //public final static CANSparkMax m_backLeft = new CANSparkMax(3, MotorType.kBrushless);
+  //public final static CANSparkMax m_backRight = new CANSparkMax(4, MotorType.kBrushless);
 
-  MecanumDrive m_robotDrive = new MecanumDrive(m_frontLeft, m_frontRight, m_backLeft, m_backRight);
+  //MecanumDrive m_robotDrive = new MecanumDrive(m_frontLeft, m_frontRight, m_backLeft, m_backRight);
   Joystick m_stick = new Joystick(0);
 
   private RobotContainer m_robotContainer;
+
+  
+  private TalonSRX motor1;
+  private TalonSRX motor2;
+  private TalonSRX motor3;
+  private TalonSRX motor4;
   
 
   @Override
   public void robotInit() {
+    // Configure motor
+    motor1 = new TalonSRX(6);
+    motor2 = new TalonSRX(7);
+    motor3 = new TalonSRX(8);
+    motor4 = new TalonSRX(9);
     
-    m_frontRight.setInverted(true);
-    m_backRight.setInverted(true);
+    TalonSRXConfiguration config = new TalonSRXConfiguration();
+    config.peakCurrentLimit = 133; // the peak current, in amps
+    config.peakCurrentDuration = 1500; // the time at the peak current before the limit triggers, in ms
+    config.continuousCurrentLimit = 120; // the current to maintain if the peak limit is triggered
+    motor1.configAllSettings(config); // apply the config settings; this selects the quadrature encoder
+    motor2.configAllSettings(config); // apply the config settings; this selects the quadrature encoder
+    motor3.configAllSettings(config); // apply the config settings; this selects the quadrature encoder
+    motor4.configAllSettings(config); // apply the config settings; this selects the quadrature encoder
+    
+    //m_frontRight.setInverted(true);
+    //m_backRight.setInverted(true);
 
   }
 
@@ -93,8 +117,12 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.driveCartesian(-m_stick.getY(), m_stick.getX(), m_stick.getZ());
+    //m_robotDrive.driveCartesian(-m_stick.getY(), m_stick.getX(), m_stick.getZ());
 
+    motor1.set(TalonSRXControlMode.PercentOutput, m_stick.getY()*0.8); // runs the motor at 80% power of the stick y
+    motor2.set(TalonSRXControlMode.PercentOutput, m_stick.getY()*0.8); // runs the motor at 80% power of the stick y
+    motor3.set(TalonSRXControlMode.PercentOutput, m_stick.getY()*0.8); // runs the motor at 80% power of the stick y
+    motor4.set(TalonSRXControlMode.PercentOutput, m_stick.getY()*0.8); // runs the motor at 80% power of the stick y
 
   }
 
