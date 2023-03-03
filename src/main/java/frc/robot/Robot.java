c691c8a8886071052ed3943f2233d36070f0007c
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2023-03-03 01:23:31 -0500

    forgot to integrate autos
    Implemented auto chooser into robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,11 +10,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  
-  private Command m_autonomousCommand;
-  double time; 
 
- private RobotContainer m_robotContainer;
+  private Command m_autonomousCommand;
+  double time;
+
+  private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
@@ -23,41 +23,44 @@ public class Robot extends TimedRobot {
     m_robotContainer.m_drive.motorBrake();
     m_robotContainer.m_shoulderpid.resetEncoder();
     m_robotContainer.m_wristSubsystem.resetEncoder();
-    //CameraServer.startAutomaticCapture("Camera Shoulder View", 1);
+    // CameraServer.startAutomaticCapture("Camera Shoulder View", 1);
     // CameraServer.startAutomaticCapture(0).setResolution(5000, 3000);
   }
 
   @Override
   public void robotPeriodic() {
-  m_robotContainer.m_shoulderpid.getDistance();
-  m_robotContainer.m_wristSubsystem.getWristEncoderDistance();
+    m_robotContainer.m_shoulderpid.getDistance();
+    m_robotContainer.m_wristSubsystem.getWristEncoderDistance();
     CommandScheduler.getInstance().run();
   }
 
   @Override
   public void disabledInit() {
-  
+
   }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
   public void autonomousInit() {
-    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    m_robotContainer.m_clawSubsystem.closeClaw(); //keeps cargo in
-    m_robotContainer.m_wristSubsystem.moveWrist(3000.0); //get value!!!!! move wrist downwards into space
-    m_robotContainer.m_clawSubsystem.openClaw(); //opens claw , lays thing down
-    wait(500);
-    m_robotContainer.m_clawSubsystem.noPowerClaw(); //gives no power to claw
-    m_robotContainer.m_drive.moveBackAuto(); //move back to get away from claw
-    m_robotContainer.m_clawSubsystem.closeClaw(); //close claw
-    wait(700); //might have to change make sure that it closes all the way
-    m_robotContainer.m_clawSubsystem.noPowerClaw(); //gives no power to claw
-    m_robotContainer.m_wristSubsystem.moveWrist(830.0); //get value!!!!! move wrist compact 
-    wait(1000);
-    m_robotContainer.m_drive.moveBackMoreAuto(); //for taxi 
-
+    // TODO: transfer this to autonomous command
+    // //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    // m_robotContainer.m_clawSubsystem.closeClaw(); //keeps cargo in
+    // m_robotContainer.m_wristSubsystem.moveWrist(3000.0); //get value!!!!! move
+    // wrist downwards into space
+    // m_robotContainer.m_clawSubsystem.openClaw(); //opens claw , lays thing down
+    // wait(500);
+    // m_robotContainer.m_clawSubsystem.noPowerClaw(); //gives no power to claw
+    // m_robotContainer.m_drive.moveBackAuto(); //move back to get away from claw
+    // m_robotContainer.m_clawSubsystem.closeClaw(); //close claw
+    // wait(700); //might have to change make sure that it closes all the way
+    // m_robotContainer.m_clawSubsystem.noPowerClaw(); //gives no power to claw
+    // m_robotContainer.m_wristSubsystem.moveWrist(830.0); //get value!!!!! move
+    // wrist compact
+    // wait(1000);
+    // m_robotContainer.m_drive.moveBackMoreAuto(); //for taxi
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -66,14 +69,15 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
     m_robotContainer.m_drive.motorBrake();
     m_robotContainer.m_shoulderpid.resetEncoder();
     m_robotContainer.m_wristSubsystem.resetEncoder();
-   // m_robotContainer.m_stopperSubsystem.retractStop(); //autnomous
+    // m_robotContainer.m_stopperSubsystem.retractStop(); //autnomous
     // wait(2000);
     // m_robotContainer.m_shoulderpid.moveArm(0);
     // m_robotContainer.m_wristSubsystem.moveWrist(830);
@@ -93,23 +97,14 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
-
-  public static void wait(int ms)
-  {
-      try
-      {
-          Thread.sleep(ms); //core java delay command
-      }
-      catch(InterruptedException ex)
-      {
-          Thread.currentThread().interrupt(); //this exception is useful to remove the glitches and errors of the thread.sleep()
-      }
+  public void simulationPeriodic() {
   }
 }
